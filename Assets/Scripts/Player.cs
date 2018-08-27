60a1e6408ccc1264d6a259859f36b8d06f64dc01
Hugo <h.lagarde@student.isartdigital.com>
2018-08-27 06:14:24 +0200

    Player, settings
    Can move player, button binding

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,51 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private CharacterController m_controller;
+    private SpriteRenderer m_spriteRenderer;
+
+    private float m_life;
+    [SerializeField]
+    private float m_verticalSpeed = 2f;
+    [SerializeField]
+    private float m_horizontalSpeed = 2f;
+
+
+    void Start () {
+        Debug.Log( Input.GetJoystickNames());
+    }
 	
-	// Update is called once per frame
 	void Update () {
-		
+        UpdateMovement();
+        UpdateButtonInput();
 	}
+
+    private void UpdateMovement()
+    {
+        float x_axis = Input.GetAxis("Horizontal");
+        float y_axis = Input.GetAxis("Vertical");
+
+        Vector3 bas_move = transform.position;
+        bas_move.x += (x_axis * m_horizontalSpeed) * Time.deltaTime;
+        bas_move.y += (y_axis * m_verticalSpeed) * Time.deltaTime;
+        transform.position = bas_move;
+    }
+
+    private void UpdateButtonInput()
+    {
+        if (Input.GetButtonDown("ButtonA"))
+            Shot();
+        if (Input.GetButtonDown("ButtonB"))
+            Shot();
+        if (Input.GetButtonDown("ButtonY"))
+            Shot();
+        if (Input.GetButtonDown("ButtonX"))
+            Shot();
+    }
+
+    private void Shot()
+    {
+        Debug.Log("instanciate projectile");
+    }
+
 }
