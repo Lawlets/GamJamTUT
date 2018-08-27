8b69bdafb9244a268d02d4fc6bccaab442163f6a
DaynerKurdi <ziad_dayner@yahoo.com>
2018-08-27 23:47:38 +0900

    Added the basic setup for powerup UI

---
+++ Assets/PowerUpTextProManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PowerUpTextProManager : MonoBehaviour
+{
+
+    [SerializeField]
+    TextMeshProUGUI[] PowerUpObjects = new TextMeshProUGUI[3];
+
+    private void Start()
+    {
+        int numberOfTextProObject = GameObject.Find("PowerUpHolder").transform.childCount;
+
+        for (int i = 0; i < numberOfTextProObject; i++)
+        {
+            PowerUpObjects[i] = GameObject.Find("PowerUpHolder").transform.GetChild(i).transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+        }
+    }
+
+}
