32538be4b09e8012c734854b6f4709603434f275
DaynerKurdi <ziad_dayner@yahoo.com>
2018-08-27 13:32:28 +0900

    Game Mamager and scene mover scrpit complated

---
+++ Assets/Scripts/SceneMover_Title.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMover_Title : MonoBehaviour
+{
+  
+
+    public void StartButtonCaller()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitButtonCaller()
+    {
+        Application.Quit();
+    }
+}
+
+
