eeb728cb73d0d2547f19ffa689931dacd7b36caf
DaynerKurdi <ziad_dayner@yahoo.com>
2018-08-28 03:56:44 +0900

    Changed the powerup from text image

--- Assets/PowerUpTextProManager.cs
+++ Assets/PowerUpTextProManager.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+//using TMPro;
+using UnityEngine.UI;
 
 public class PowerUpTextProManager : MonoBehaviour
 {
@@ -10,8 +11,14 @@ public class PowerUpTextProManager : MonoBehaviour
 
     PowerUpManager pm;
 
+    //[SerializeField]
+    //TextMeshProUGUI[] PowerUpObjects = new TextMeshProUGUI[3];
+
     [SerializeField]
-    TextMeshProUGUI[] PowerUpObjects = new TextMeshProUGUI[3];
+    Image[] imageObject = new Image[3];
+
+    [SerializeField]
+    Sprite[] numberSprite = new Sprite[3];
 
     private void Start()
     {
@@ -21,7 +28,9 @@ public class PowerUpTextProManager : MonoBehaviour
 
         for (int i = 0; i < numberOfTextProObject; i++)
         {
-            PowerUpObjects[i] = GameObject.Find("PowerUpHolder").transform.GetChild(i).transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+            //PowerUpObjects[i] = GameObject.Find("PowerUpHolder").transform.GetChild(i).transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+
+            imageObject[i] = GameObject.Find("PowerUpHolder").transform.GetChild(i).transform.GetChild(2).GetComponent<Image>();
         }
 
         dm.powerUp_Water_Delegate += updateTheWaterText;
@@ -52,16 +61,19 @@ public class PowerUpTextProManager : MonoBehaviour
         
         if (pm.GetPowerUpLevel_Water() == 1)
         {
-            PowerUpObjects[0].text = "1";
+            //  PowerUpObjects[0].text = "1";
+            imageObject[0].sprite = numberSprite[0];
         }
         else if(pm.GetPowerUpLevel_Water() == 2)
         {
-            PowerUpObjects[0].text = "2";
+            //  PowerUpObjects[0].text = "2";
+            imageObject[0].sprite = numberSprite[1];
         }
 
         else if(pm.GetPowerUpLevel_Water() == 3)
         {
-            PowerUpObjects[0].text = "3";
+            //  PowerUpObjects[0].text = "3";
+            imageObject[0].sprite = numberSprite[2];
         } 
     }
 
@@ -70,16 +82,19 @@ public class PowerUpTextProManager : MonoBehaviour
 
         if (pm.GetPowerUpLevel_Snow() == 1)
         {
-            PowerUpObjects[1].text = "1";
+            //  PowerUpObjects[1].text = "1";
+            imageObject[1].sprite = numberSprite[0];
         }
         else if (pm.GetPowerUpLevel_Snow() == 2)
         {
-            PowerUpObjects[1].text = "2";
+            // PowerUpObjects[1].text = "2";
+            imageObject[1].sprite = numberSprite[1];
         }
 
         else if (pm.GetPowerUpLevel_Snow() == 3)
         {
-            PowerUpObjects[1].text = "3";
+            // PowerUpObjects[1].text = "3";
+            imageObject[0].sprite = numberSprite[2];
         }
     }
 
@@ -88,16 +103,19 @@ public class PowerUpTextProManager : MonoBehaviour
 
         if (pm.GetPowerUpLevel_Thunder() == 1)
         {
-            PowerUpObjects[2].text = "2";
+            // PowerUpObjects[2].text = "2";
+            imageObject[2].sprite = numberSprite[0];
         }
         else if (pm.GetPowerUpLevel_Thunder() == 2)
         {
-            PowerUpObjects[2].text = "2";
+            //PowerUpObjects[2].text = "2";
+            imageObject[2].sprite = numberSprite[1];
         }
 
         else if (pm.GetPowerUpLevel_Thunder() == 3)
         {
-            PowerUpObjects[2].text = "3";
+            //PowerUpObjects[2].text = "3";
+            imageObject[2].sprite = numberSprite[2];
         }
     }
 
