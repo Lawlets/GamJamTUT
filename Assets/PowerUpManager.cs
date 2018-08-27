4d59e61e5c1425780cfeef1ef276a4f98e45d7b7
DaynerKurdi <ziad_dayner@yahoo.com>
2018-08-28 00:21:37 +0900

    UP power Setup

---
+++ Assets/PowerUpManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpManager : MonoBehaviour {
+
+	public enum POWERUPLEVEL
+    {
+        levelOne = 1,
+
+        levelTwo = 2,
+
+        LevelThree = 3
+    }
+
+    public POWERUPLEVEL[] playerPowerUpLevel = new POWERUPLEVEL[3];
+
+    private int[] currentPowerLevel = new int[3];
+
+    private void Start()
+    {
+        for (int i = 0; i < playerPowerUpLevel.Length; i++)
+        {
+            playerPowerUpLevel[i] = POWERUPLEVEL.levelOne;
+            currentPowerLevel[i] = 1;
+        }
+    }
+
+
+    public int GetPowerUpLevel_Water()
+    {
+        return currentPowerLevel[0];
+    }
+
+    public int GetPowerUpLevel_Snow()
+    {
+        return currentPowerLevel[1];
+    }
+
+    public int GetPowerUpLevel_Thunder()
+    {
+        return currentPowerLevel[2];
+    }
+}
